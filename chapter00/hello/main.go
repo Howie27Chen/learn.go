a1a15356e5d138d821a2dc0d158c8da62e9548f4
Howie27Chen <15820583113@163.com>
2022-11-14 23:13:50 +0800

    add(hello): the first hello go program.

---
+++ chapter00/hello/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello, Golang!")
+	fmt.Println("This is the first line")
+	fmt.Println("This is the second line")
+}
+
+// to run the program: go run main.go
+// to build the program: go build main.go
