f1e8ae6d09df499c91070f80c28f6e76dc809c87
Howie27Chen <15820583113@163.com>
2022-11-20 19:51:47 +0800

    refine(app-bodyfat): refine with func.

--- app-body-fat/main.go
+++ app-body-fat/main.go
@@ -3,68 +3,81 @@ package main
 import "fmt"
 
 func main() {
-	var (
-		name   string
-		gender string
-		age    int
-		height float64
-		weight float64
-	)
 
 	for {
-		fmt.Print("姓名: ")
-		fmt.Scanln(&name)
-		fmt.Print("性别（男/女）: ")
-		fmt.Scanln(&gender)
-		fmt.Print("年龄: ")
-		fmt.Scanln(&age)
-		fmt.Print("身高（米）: ")
-		fmt.Scanln(&height)
-		fmt.Print("体重（公斤）: ")
-		fmt.Scanln(&weight)
-
-		bmi := weight / (height * height)
-		fmt.Printf("BMI 指数是: %.2f\n", bmi)
-
-		var gender_weight float64
-		if gender == "男" {
-			gender_weight = 1
-		} else {
-			gender_weight = 0
-		}
-		bodyfat := 1.2*bmi + 0.23*float64(age) - 5.4 - 10.8*gender_weight
-		fmt.Printf("体脂率是: %.2f%s\n", bodyfat, "%")
-
-		if age >= 18 && age <= 39 {
-			if bodyfat <= 10 {
-				fmt.Println("状态: 偏瘦")
-			} else if bodyfat > 10 && bodyfat <= 16 {
-				fmt.Println("状态: 标准")
-			} else if bodyfat > 16 && bodyfat <= 24 {
-				fmt.Println("状态: 偏重")
-			} else if bodyfat > 24 && bodyfat <= 26 {
-				fmt.Println("状态: 肥胖")
-			} else {
-				fmt.Println("状态: 严重肥胖")
-			}
-
-		} else if age >= 40 && age <= 59 {
-			// todo
-			fmt.Println("暂时不支持计算: ", age, "岁的人群")
-		} else if age >= 60 {
-			// todo
-			fmt.Println("暂时不支持计算: ", age, "岁的人群")
-		} else {
-			fmt.Println("超出计算范围")
-		}
-
-		cal_continue := "是"
-		fmt.Print("是否继续输入(是/否): ")
-		fmt.Scanln(&cal_continue)
-		if cal_continue != "否" {
-			continue
-		} else {
+		name, age, gender, height, weight := getInputINfo()
+		bmi := calBMI(height, weight)
+		bodyfat := calBodyFat(bmi, age, gender)
+		fmt.Printf("%s 体脂率是 %.2f%s\n", name, bodyfat, "%")
+		evaluateBodyFat(name, age, gender, bodyfat)
+		if inputAgain() {
 			break
 		}
+	}
+}
+
+func getInputINfo() (name string, age int, gender string, height float32, weight float32) {
+
+	fmt.Print("姓名: ")
+	fmt.Scanln(&name)
+	fmt.Print("年龄: ")
+	fmt.Scanln(&age)
+	fmt.Print("性别（男/女）: ")
+	fmt.Scanln(&gender)
+	fmt.Print("身高（米）: ")
+	fmt.Scanln(&height)
+	fmt.Print("体重（公斤）: ")
+	fmt.Scanln(&weight)
+	return name, age, gender, height, weight
+
+}
+
+func calBMI(height float32, weight float32) float32 {
+	return weight / (height * height)
+}
+
+func calBodyFat(bmi float32, age int, gender string) float32 {
+	var powerGender float32
+	if gender == "男" {
+		powerGender = 1
+	} else {
+		powerGender = 0
+	}
+	return 1.2*bmi + 0.23*float32(age) - 5.4 - 10.8*powerGender
+}
+
+func evaluateBodyFat(name string, age int, gender string, bodyfat float32) {
+	if age >= 18 && age <= 39 {
+		if bodyfat <= 10 {
+			fmt.Println("状态: 偏瘦")
+		} else if bodyfat > 10 && bodyfat <= 16 {
+			fmt.Println("状态: 标准")
+		} else if bodyfat > 16 && bodyfat <= 24 {
+			fmt.Println("状态: 偏重")
+		} else if bodyfat > 24 && bodyfat <= 26 {
+			fmt.Println("状态: 肥胖")
+		} else {
+			fmt.Println("状态: 严重肥胖")
+		}
+
+	} else if age >= 40 && age <= 59 {
+		// todo
+		fmt.Println("暂时不支持计算: ", age, "岁的人群")
+	} else if age >= 60 {
+		// todo
+		fmt.Println("暂时不支持计算: ", age, "岁的人群")
+	} else {
+		fmt.Println("超出计算范围")
+	}
+}
+
+func inputAgain() bool {
+	cal_continue := "是"
+	fmt.Print("是否继续输入(是/否): ")
+	fmt.Scanln(&cal_continue)
+	if cal_continue != "否" {
+		return false
+	} else {
+		return true
 	}
 }
