fa583a80a5b965e620941cd32fa0a7c8e3f98c41
Howie27Chen <15820583113@163.com>
2022-11-28 07:43:46 +0800

    update(constants)

--- go-by-example/03-constants.go
+++ go-by-example/03-constants.go
@@ -5,6 +5,22 @@ import (
 	"math"
 )
 
+type myInt int
+
+const nn myInt = 13
+
+// const m int = nn + 15
+// 无类型常量
+const m = nn + 15
+
+// iota 实现枚举
+const (
+	_ = iota
+	one
+	two
+	three
+)
+
 const s string = "constant"
 
 func main() {
@@ -13,6 +29,11 @@ func main() {
 	const n = 50000000
 
 	const d = 3e20 / n
+	// 常量具有严格的数据类型
+	//var a int = 5
+	//fmt.Println("const:", nn+a)
+	fmt.Println(one, two, three)
+
 	fmt.Println(d)
 
 	fmt.Println(int64(d))
