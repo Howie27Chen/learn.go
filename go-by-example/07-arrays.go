2bab30211d709cd5f45c5894f010e92e5edd3132
Howie27Chen <15820583113@163.com>
2022-11-28 08:00:13 +0800

    update(arrays)

--- go-by-example/07-arrays.go
+++ go-by-example/07-arrays.go
@@ -1,9 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"unsafe"
+)
+
+// 数组定义：Go 语言的数组是一个长度固定，由同构类型元素组成的连续序列
+// var arr [N]T
+
+func foo(arr [5]int) {
+}
 
 func main() {
 	var a [5]int
+
+	var arr1 [5]int
+	// var arr2 [6]int
+	//var arr3 [5]string
+	foo(arr1)
+	// foo(arr2) // 长度属性不一致
+	//foo(arr3) // 元素类型属性不同
+	var arr = [6]int{1, 2, 3, 4, 5, 6}
+	fmt.Println("数组长度: ", len(arr))
+	fmt.Println("数组大小: ", unsafe.Sizeof(arr))
+
+	// ... 忽略数组长度
+	var arr_init = [...]int{1, 2, 3, 4, 5, 6}
+	fmt.Printf("type: %T\n", arr_init) // [6]int
+
 	fmt.Println("empty 5 int arrays: ", a)
 
 	a[4] = 100
