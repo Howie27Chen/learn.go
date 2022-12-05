f9867d7395529c6ceeeb3565d0d4bc66ab6b53ad
Howie27Chen <15820583113@163.com>
2022-12-05 08:09:50 +0800

    update(for)

--- go-by-example/04-for.go
+++ go-by-example/04-for.go
@@ -2,26 +2,33 @@ package main
 
 import "fmt"
 
-func main() {
-	i := 1
-	for i <= 3 {
-		fmt.Println(i)
-		i = i + 1
+func show_basic() {
+
+	for i := 0; i < 3; i++ {
+		fmt.Println("i = ", i)
 	}
 
-	for j := 7; j <= 9; j++ {
-		fmt.Println(j)
+	j := 0
+	for j < 3 {
+		fmt.Println("j = ", j)
+		j += 1
 	}
 
-	for {
-		fmt.Println("loop")
-		break
-	}
-
-	for n := 0; n <= 5; n++ {
+	for n := 0; n < 5; n++ {
 		if n%2 == 0 {
 			continue
 		}
-		fmt.Println(n)
+		fmt.Println("n = ", n)
 	}
+
+	for {
+		fmt.Println("loop break")
+		break
+	}
+}
+
+func main() {
+
+	show_basic()
+
 }
