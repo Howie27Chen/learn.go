1163850746a6c2c483ac9db63090485fb8b7bb20
Howie27Chen <15820583113@163.com>
2022-12-09 22:36:19 +0800

    add(func): 可变参数展示

--- go-by-example/11-func.go
+++ go-by-example/11-func.go
@@ -10,10 +10,32 @@ func plusPlus(a, b, c int) int {
 	return a + b + c
 }
 
+// show_var_param 展示函数的可变参数
+func show_var_param(sl []int, elements ...int) []int {
+	// 可变参数是基于切片实现的
+	fmt.Printf("%T\n", elements)
+	if len(elements) == 0 {
+		println("no element to append")
+		return sl
+	}
+	sl = append(sl, elements...)
+	return sl
+}
+
 func main() {
 	res := plus(1, 2)
 	fmt.Println("1 + 2 = ", res)
 
 	res = plusPlus(1, 2, 3)
 	fmt.Println("1 + 2 + 3 = ", plusPlus(1, 2, 3))
+
+	sl := []int{1, 2, 3}
+	fmt.Println("before: sl = ", sl)
+	// 可变参数可以是 0  个
+	sl = show_var_param(sl)
+	fmt.Println("after var none: sl = ", sl)
+	// 可变参数可以是多个
+	sl = show_var_param(sl, 1, 2, 3)
+	fmt.Println("after var 1, 2, 3: sl = ", sl)
+
 }
