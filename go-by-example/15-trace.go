9cfa92e763acd7b9b3d04a440fc81b346449fce9
Howie27Chen <15820583113@163.com>
2022-12-12 07:55:07 +0800

    add(trace): 首次添加获取 goroutine routine ID 的函数。

--- go-by-example/15-trace.go
+++ go-by-example/15-trace.go
@@ -1,9 +1,31 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"runtime"
+	"strconv"
 )
+
+var gorountineSpace = []byte("goroutine ")
+
+// curGoroutineID 获取 goroutine 的 ID 号
+func curGoroutineID() uint64 {
+	b := make([]byte, 64)
+	b = b[:runtime.Stack(b, false)]
+	// Parse the 4707 out of "goroutine 4707["
+	b = bytes.TrimPrefix(b, gorountineSpace)
+	i := bytes.IndexByte(b, ' ')
+	if i < 0 {
+		panic(fmt.Sprintf("No space found in %q", b))
+	}
+	b = b[:i]
+	n, err := strconv.ParseUint(string(b), 10, 64)
+	if err != nil {
+		panic(fmt.Sprintf("Failed to parse goroutine ID out of %q: %v", b, err))
+	}
+	return n
+}
 
 func Trace() func() {
 	pc, _, _, ok := runtime.Caller(1)
@@ -14,10 +36,11 @@ func Trace() func() {
 
 	fn := runtime.FuncForPC(pc)
 	name := fn.Name()
+	gid := curGoroutineID()
 
-	fmt.Println("enter:", name)
+	fmt.Sprintf("g[%05d]: enter: [%s]\n", gid, name)
 	return func() {
-		fmt.Println("exit", name)
+		fmt.Printf("g[%05d]: exit: [%s]\n", gid, name)
 	}
 
 }
